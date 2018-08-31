6412dbf0f6eecf8ca5a231757f04ab30e17b106f
Jordan Griffin <jordan.griffin@asynchrony.com>
2018-08-31 15:28:21 -0500

    created podfile

--- Momspo/MomspoTests/MomspoTests.swift
+++ Momspo/MomspoTests/MomspoTests.swift
@@ -1,36 +1,8 @@
-//
-//  MomspoTests.swift
-//  MomspoTests
-//
-//  Created by jordan.griffin on 8/31/18.
-//  Copyright © 2018 lilybug. All rights reserved.
-//
-
 import XCTest
 @testable import Momspo
 
 class MomspoTests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        print("Test runs!")
     }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
 }
